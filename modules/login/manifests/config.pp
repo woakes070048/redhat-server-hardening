5ae44acd5eb5bd56d9dda1f9e69ac58866ee9f4c
Devyn Spencer <devynspencer@users.noreply.github.com>
2014-11-13 14:15:43 -0800

    Added lens to login.defs config entries

--- modules/login/manifests/config.pp
+++ modules/login/manifests/config.pp
@@ -3,12 +3,14 @@ class { 'login.defs':
 
   augeas { 'MD5_CRYPT_ENAB':
     context => $::login::params::config_file,
+    lens    => 'login_defs',
     changes => 'set MD5_CRYPT_ENAB no',
     onylif  => 'match MD5_CRYPT_ENAB not_include no',
   }
 
   augeas { 'ENCRYPT_METHOD':
     context => $::login::params::config_file,
+    lens    => 'login_defs',
     changes => 'set ENCRYPT_METHOD SHA512',
     onlyif  => 'match ENCRYPT_METHOD not_include SHA512',
   }
