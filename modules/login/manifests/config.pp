250b2324dd6fb5050a5eb2933e2a1e0e46f6f8d1
Devyn Spencer <devyn@Hrovitnir.local>
2014-11-13 16:13:28 -0800

    Fixed syntax error inside of augeas onlyif conditions

--- modules/login/manifests/config.pp
+++ modules/login/manifests/config.pp
@@ -5,13 +5,13 @@ class { 'login.defs':
     context => $::login::params::config_file,
     lens    => 'login_defs',
     changes => 'set MD5_CRYPT_ENAB no',
-    onylif  => 'match MD5_CRYPT_ENAB not_include no',
+    onylif  => 'match MD5_CRYPT_ENAB not_include "no"',
   }
 
   augeas { 'ENCRYPT_METHOD':
     context => $::login::params::config_file,
     lens    => 'login_defs',
     changes => 'set ENCRYPT_METHOD SHA512',
-    onlyif  => 'match ENCRYPT_METHOD not_include SHA512',
+    onlyif  => 'match ENCRYPT_METHOD not_include "SHA512"',
   }
 }
