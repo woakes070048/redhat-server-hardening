232e078cb6ac83447816fc9b59c64a47e2cc2c00
Devyn Spencer <devyn@Hrovitnir.local>
2014-11-14 14:34:32 -0800

    Refactored homeperms  module and combined with nfs module

--- modules/nfs/manifests/config.pp
+++ modules/nfs/manifests/config.pp
@@ -1,7 +1,18 @@
 # == Class nfs::config
 #
-# This class is called from nfs
+# Disables global write to /home directory.
 #
 class nfs::config {
+  file { '/home':
+    mode         => '0700',
+    recurse      => true,
+    recurselimit => '1',
+    ignore       => '/home';
+  }
 
+  exec { '/home mode':
+    command => 'chmod 755 /home',
+    user    => 'root',
+    require => File['/home'],
+  }
 }
