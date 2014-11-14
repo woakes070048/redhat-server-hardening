7b49bb64abbab61d584b4f765bf6fbe09c2d7502
Devyn Spencer <devyn@Hrovitnir.local>
2014-11-14 15:18:06 -0800

    Refactored execshield module

--- modules/execshield/manifests/init.pp
+++ modules/execshield/manifests/init.pp
@@ -1,31 +1,18 @@
+# == Class: execshield
 #
-# Module: execshield
-#
-# Class: execshield
-#
-# Description:
-#       This class makes sure execshield is enabled
-#
-# Defines:
-#       None
+# Installs and configures execshield according to USGCB best practices
 #
 # LinuxGuide:
-#       2.2.4.3
+# 2.2.4.3
 #
 # CCERef#:
-#       CCE-4168-1
+# CCE-4168-1
 #	CCE-4146-7
 #
-class execshield {
-	# GuideSection 2.2.4.3
-	# CCE-4168-1, CCE-4146-7
-	# Enable execshield
-	augeas::basic-change {"Enable ExecShield, 2.2.4.3": 
-		file=>"/etc/sysctl.conf", 
-		lens=>"sysctl.lns", 
-		changes => [
-			"set kernel.exec-shield 1",
-			"set kernel.randomize_va_space 1", 
-		]
-        }
+class execshield (
+
+) inherits execshield::params {
+
+  class { '::execshield::config': } ~>
+  Class['::execshield']
 }
